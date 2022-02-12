4d95412198a1079e38f570833dfacd264b1f21be
Yauheni Baradzko <Yauheni.Baradzko@itechart-group.com>
2022-02-12 13:11:10 +0100

    feat: connect app to the database

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const db = require('./db')
 
 const PORT = process.env.PORT || 8080
 
@@ -12,7 +13,13 @@ app.use(cookieParser())
 app.use(cors())
 
 const start = async () => {
-  app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  try {
+    await db.authenticate()
+    await db.sync()
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 start()
